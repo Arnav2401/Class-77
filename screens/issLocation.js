53ecf760604c61287e336c9fd9fd99c720b0ce37
Arnav2401 <77386547+Arnav2401@users.noreply.github.com>
2021-07-27 18:13:54 +0530

    Add files via upload

---
+++ screens/issLocation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+
+export default class ISSLocator extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                <Text>
+                    SecondScreen
+                </Text>
+            </View>
+        )
+    }
+}
